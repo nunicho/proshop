6fed5bb87c6eb26e0b8de02779def603b9b56a11
MauricioAlonso <program.alonso@gmail.com>
2024-05-31 22:16:31 -0300

    Listo

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,19 +4,19 @@ const users = [
   {
     name: "Admin User",
     email: "admin@email.com",
-    password: bcrypt.hashSync("12345", 20),
+    password: bcrypt.hashSync("12345", 10),
     isAdmin: true,
   },
   {
     name: "John Doe",
     email: "admin@email.com",
-    password: bcrypt.hashSync("12345", 20),
+    password: bcrypt.hashSync("12345", 10),
     isAdmin: false,
   },
   {
     name: "Jane Doe",
     email: "jane@email.com",
-    password: bcrypt.hashSync("12345", 20),
+    password: bcrypt.hashSync("12345", 10),
     isAdmin: false,
   },
 ];
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,33 @@
-import  express from 'express'
-import dotenv from 'dotenv'
-dotenv.config();
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import productRoutes from './routes/productRoutes.js'
+    import  express from 'express'
+    import dotenv from 'dotenv'
+    import cookieParser from 'cookie-parser'
+    dotenv.config();
+    import connectDB from './config/db.js';
+    import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+    import productRoutes from './routes/productRoutes.js'
+    import userRoutes from "./routes/userRoutes.js";
 
-const port = process.env.PORT || 5000;
+    const port = process.env.PORT || 5000;
 
-connectDB()
+    connectDB()
 
-const app = express();
+    const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-});
+    // Body parser middleware
+    app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
 
-app.use('/api/products', productRoutes)
+    //Cookie parser middleware
+    app.use(cookieParser())
 
-app.use(notFound)
-app.use(errorHandler)
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    });
 
-app.listen(port, ()=> console.log(`Server is running on port ${port}`))
+    app.use('/api/products', productRoutes)
+    app.use("/api/users", userRoutes);
+
+    app.use(notFound)
+    app.use(errorHandler)
+
+    app.listen(port, ()=> console.log(`Server is running on port ${port}`))
