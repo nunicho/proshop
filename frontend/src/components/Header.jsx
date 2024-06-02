5b10ac38b0f511caed360d4247d6c2c81fcae797
MauricioAlonso <program.alonso@gmail.com>
2024-06-02 20:36:36 -0300

    listo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 //import {Navbar, Nav, Container} from 'react-bootstrap'
-import { LinkContainer } from "react-router-bootstrap";
+import { useNavigate } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Badge from "react-bootstrap/Badge";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import {useSelector, useDispatch} from "react-redux"
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import {logout} from "../slices/authSlice"
 import logo from "../assets/logo.png";
-import {useSelector} from "react-redux"
 
 
 const Header = () => {
@@ -16,8 +19,19 @@ const Header = () => {
   const {cartItems} = useSelector((state)=> state.cart)
   const {userInfo} = useSelector((state) => state.auth);
 
-  const logoutHandler = () =>{
-    console.log('logout')
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation()
+
+  const logoutHandler = async () =>{
+   try {
+    await logoutApiCall().unwrap();
+    dispatch(logout())
+    navigate('/login')
+   } catch (err) {
+    console.log(err)
+   }
   }
 
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
 import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
 
 
 const router = createBrowserRouter(
@@ -25,7 +26,8 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
-      <Route path='/login' element={<LoginScreen />} /> 
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
